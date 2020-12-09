0751e83ab7108d562ed4004b1df2063a6848fc14
Alex Filipenko <alexfilipenko719@gmail.com>
2020-12-09 10:29:05 +0200

    Updated countdown

--- js/countdown.js
+++ js/countdown.js
@@ -3,7 +3,7 @@ const second = 1000,
   hour = minute * 60,
   day = hour * 24;
 
-let countDown = new Date('Oct 15, 2019 00:00:00').getTime(),
+let countDown = new Date('Jan 01, 2021 00:00:00').getTime(),
   x = setInterval(function () {
 
     let now = new Date().getTime(),
